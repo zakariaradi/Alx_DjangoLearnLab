0d0417216985640fbc3f07c784fa6a245791af7d
zakaria.r <zakariaradi.dev@outlook.com>
2026-01-18 17:52:54 +0100

    Add user authentication with login, logout, and registration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books
+from .views import LibraryDetailView
+from .views import register
+from .views import CustomLoginView
+from .views import CustomLogoutView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    path('register/', register, name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+
 from .models import Library
 from .models import Book
+
 
 # Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+
+# Class-based view: library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+# Register view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
+# Login view (Class-based)
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+
+# Logout view (Class-based)
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
 
