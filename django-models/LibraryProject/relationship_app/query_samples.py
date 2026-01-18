ed16b9e1e6e7298e7ee402d7761b0800506cce5c
zakaria.r <zakariaradi.dev@outlook.com>
2026-01-18 16:51:06 +0100

    Fix ORM queries to satisfy relationship checker requirements

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,9 +1,11 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
+
 # Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    books = Book.objects.filter(author=author)
+    return books
 
 
 # List all books in a library
@@ -15,4 +17,6 @@ def get_books_in_library(library_name):
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    librarian = Librarian.objects.get(library=library)
+    return librarian
+
