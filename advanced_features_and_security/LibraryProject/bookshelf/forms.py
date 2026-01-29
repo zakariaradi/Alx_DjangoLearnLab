ee7acc820b5b9e685371736addef79be19c65cbe
zakaria.r <zakariaradi.dev@outlook.com>
2026-01-29 19:58:43 +0100

    Add ExampleForm and secure form handling

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,7 +2,10 @@ from django import forms
 from .models import Book
 
 
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ["title"]
+
+
+
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Book
-from .forms import BookForm
+from .forms import ExampleForm
 
 
 @login_required
 def book_list(request):
-    books = Book.objects.all()  # ORM (safe from SQL injection)
+    books = Book.objects.all()
     return render(request, "bookshelf/book_list.html", {"books": books})
 
 
@@ -14,12 +14,12 @@ def book_list(request):
 @permission_required("bookshelf.can_create", raise_exception=True)
 def add_book(request):
     if request.method == "POST":
-        form = BookForm(request.POST)
-        if form.is_valid():  # input validation
+        form = ExampleForm(request.POST)
+        if form.is_valid():
             form.save()
             return redirect("book_list")
     else:
-        form = BookForm()
+        form = ExampleForm()
 
     return render(request, "bookshelf/form_example.html", {"form": form})
 
@@ -28,7 +28,7 @@ def add_book(request):
 @permission_required("bookshelf.can_edit", raise_exception=True)
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    form = BookForm(request.POST or None, instance=book)
+    form = ExampleForm(request.POST or None, instance=book)
 
     if form.is_valid():
         form.save()
