43b3f5bfd88d1152d440c06aeb19314772dfedb0
zakaria.r <zakariaradi.dev@outlook.com>
2026-01-31 17:59:21 +0100

    Include api.urls in project URL configuration

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,11 +1,11 @@
-from rest_framework.generics import ListAPIView
-from .models import Book
-from .serializers import BookSerializer
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+]
 
-class BookList(ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
 
 
 
