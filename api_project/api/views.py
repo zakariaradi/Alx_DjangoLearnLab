60b62133b5d52890787567ea064ff8c26785a176
zakaria.r <zakariaradi.dev@outlook.com>
2026-01-31 17:48:41 +0100

    update

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
@@ -6,3 +6,4 @@ from .serializers import BookSerializer
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-]
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
 
 
