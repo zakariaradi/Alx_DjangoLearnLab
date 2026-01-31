dfdc9ec45542e754879368ea4ddafa3e8d6027a4
zakaria.r <zakariaradi.dev@outlook.com>
2026-01-31 17:55:32 +0100

    Fix BookList to extend generics.ListAPIView for checker

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,10 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
 
