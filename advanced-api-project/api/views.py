e9a7e3e1aed39c6ab764477cf1c136a81f25c691
zakaria.r <zakariaradi.dev@outlook.com>
2026-02-05 18:46:40 +0100

    Fix filtering imports to satisfy ALX checker

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,10 +1,12 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Book
 from .serializers import BookSerializer
+
 
 
 class BookListView(generics.ListAPIView):
