2daee0ee99490aa7d0629e6946843f578b5b825b
zakaria.r <zakariaradi.dev@outlook.com>
2026-02-05 18:22:24 +0100

    Fix Book update and delete URL paths to satisfy checker

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -10,7 +10,8 @@ from .views import (
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
