84710d76bda0ac0bbc91b506c863370064e46089
zakaria.r <zakariaradi.dev@outlook.com>
2026-02-18 17:23:14 +0000

    Update RegisterSerializer to match checker requirements

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User
