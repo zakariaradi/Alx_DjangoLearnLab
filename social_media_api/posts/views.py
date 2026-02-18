9a846665aa29cd0ea1356c73c27aa95709de655b
zakaria.r <zakariaradi.dev@outlook.com>
2026-02-18 18:04:22 +0000

    Implement feed view using following relationship with ordered posts

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,34 +1,18 @@
-from rest_framework import viewsets, permissions
-from rest_framework.exceptions import PermissionDenied
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def feed_view(request):
+    following_users = request.user.following.all()
 
+    posts = Post.objects.filter(
+        author__in=following_users
+    ).order_by('-created_at')
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-
-    def perform_create(self, serializer):
-        post = serializer.validated_data.get('post')
-
-        if not post:
-            raise PermissionDenied("Post is required.")
-
-        serializer.save(author=self.request.user)
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
